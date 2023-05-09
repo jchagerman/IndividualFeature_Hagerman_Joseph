c53da6d6517d8a43bf53e6b17c51cb554f5a671b
Joseph Hagerman <jchagerman@mail.csuchico.edu>
2023-05-09 08:52:33 -0700

    Added more generator items

---
+++ IndividualFeature_Hagerman_Joseph/Assets/Scripts/Generator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject Destroyed;
+    public GameObject Active;
+    /// <summary>
+    /// true = BadFuture, false = GoodFuture
+    /// </summary>
+    public bool FutureType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (true)
+        {
+
+        }
+    }
+}
