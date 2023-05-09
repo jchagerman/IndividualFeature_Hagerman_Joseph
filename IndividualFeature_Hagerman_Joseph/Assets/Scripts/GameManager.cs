80da1b1323bd770438d04c01ef3c60d7c23164b7
Joseph Hagerman <jchagerman@mail.csuchico.edu>
2023-05-09 09:00:09 -0700

    Update GameManager.cs

--- IndividualFeature_Hagerman_Joseph/Assets/Scripts/GameManager.cs
+++ IndividualFeature_Hagerman_Joseph/Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// 1 = Present, 2 = past, 3 = GoodFuture, 4 = BadFuture
+    /// </summary>
     public int TimeZone;
     public bool generatorDestroyed = false;
     public bool PastActive;
